fcf5249fb9da7beb40b13b34d2b13a95878cea6b
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-20 21:27:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,24 +1,13 @@
-#class Anagram 
- # attr_accessor :words
+class Anagram 
+  attr_accessor :words
   
-  #def initialize(words)
-   # @words = words
-  #end
+  def initialize(words)
+   @words = words
+  end
   
-  #def match(array)
-   # array.select do |word| 
-    #@words.split.("").sort == word.split("").sort
-    #end
-  #end
-#end
-class Anagram
- attr_accessor :word
- def initialize(word)
-   @word = word
- end
- def match(words_array)
-   words_array.select do |word_check|
-   @word.split(“”).sort == word_check.split(“”).sort
-   end
- end
+  def match(array)
+   array.select do |word| 
+    @words.split.("").sort == word.split("").sort
+    end
+  end
 end
